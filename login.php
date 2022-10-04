b71d4123dd2f8b0b1a208ea5e7267a72bc52dda8
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-04 23:52:58 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,10 +7,9 @@
     ?>
 
 <?php
-    header("Refresh:0; url=login.php");
 	if(isset($_GET['pesan'])){
 		if($_GET['pesan']=="gagal"){
-			echo "<div class='alert alert-danger'> Data Yang Dimasukan Salah !</div>";
+			echo "<div class='alert alert-danger'>Krede!</div>";
 		}
 	}
 ?>
