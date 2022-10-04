101416385290f9dccba565e9e5f5d9ab02078188
reikochn <norarenada@students.undip.ac.id>
2022-10-05 00:56:03 +0700

    Update daftar.php
    ganti warna

--- daftar.php
+++ daftar.php
@@ -50,7 +50,7 @@
                 placeholder="Password"
               />
               <button
-                class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                class="mt-5 tracking-wide font-semibold bg-green-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-800 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
               >
                 <svg
                   class="w-6 h-6 -ml-2"
