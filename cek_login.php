a9463ec9cc1f82facddf9e3fb5358a5cc5d375d3
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-04 21:39:37 +0700

    adding cek admin and user

--- cek_login.php
+++ cek_login.php
@@ -12,6 +12,31 @@ $password = $_POST['password'];
 $login = mysqli_query($db,"select from akun where username='$username' and password='$password'");
 
 //menghitung jumlah data yang ditemukan
-$cek = mysqli_num_rows($login)
+$cek = mysqli_num_rows($login);
 
+//cek apakah username dan password ditemukan pada database
+if($cek>0){
+
+    //memasukkan data dari login ke $data dalam bentuk array
+    $data=mysqli_fetch_assoc($login);
+
+    //cek jika user login sebagai admin
+    if($data['username']=="admin"){
+
+        //buat session login dan username
+        $_SESSION['username'] = $username;
+        //alihkan ke halaman dashboard admin
+        header("location:dashboard_admin.php");
+    }
+    else if($data['username']!="admin"){
+
+        //buat session login dan username
+        $_SESSION['username'] = $username;
+        //alihkan ke home customer
+        header("location:bookroom.php");
+    }
+    else{
+        //
+    }
+}
 ?>
