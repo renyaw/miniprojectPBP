c9348574878515f4e8670f2553e90fab000c95c3
felliiptr <79779482+felliiptr@users.noreply.github.com>
2022-10-04 23:51:40 +0700

    apasih
    yaAllah

---
+++ get_ruangan.php
@@ -0,0 +1,24 @@
+<script type="text/javascript" src="script.js"></script>
+<?php 
+  require_once("db_login.php");
+
+  if (isset($_GET["id_tipe"])) {
+    $id_tipe = $_GET["id_tipe"];
+
+    $query = "SELECT * FROM model WHERE id_tipe='" . $id_tipe . "'";
+    $result = $db->query($query);
+
+    if (!$result) {
+      die("Could not query the database: <br />" . $db->error);
+    }
+
+    echo '<option value="" selected>Pilih Room</option>';
+    
+    while ($row = $result->fetch_object()) {
+      echo '<option value="' . $row->id . '">' . $row->nama . '</option>';
+    }
+
+    $result->free();
+    $db->close();
+  }
+?>
