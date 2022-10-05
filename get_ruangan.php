a2fbd28f5d2cc743659990fdce5145d3bca9f8cb
imadearya <imadearya1@gmail.com>
2022-10-05 09:00:03 +0700

    Update get_ruangan.php

--- get_ruangan.php
+++ get_ruangan.php
@@ -24,29 +24,29 @@
 ?>
 <?php 
 // Punya Nada
-  require_once("db_login.php");
+//   require_once("db_login.php");
 
-  if (isset($_GET["id_tipe"])) {
-    $id_tipe = $_GET["id_tipe"];
+//   if (isset($_GET["id_tipe"])) {
+//     $id_tipe = $_GET["id_tipe"];
 
-    $query = "SELECT * FROM ruang WHERE id_tipe='" . $id_tipe . "'";
-    $result = $db->query($query);
+//     $query = "SELECT * FROM ruang WHERE id_tipe='" . $id_tipe . "'";
+//     $result = $db->query($query);
 
-    if (!$result) {
-      die("Could not query the database: <br />" . $db->error);
-    }
+//     if (!$result) {
+//       die("Could not query the database: <br />" . $db->error);
+//     }
 
-    echo '<option value="" selected>Pilih Room</option>';
+//     echo '<option value="" selected>Pilih Room</option>';
     
-    while ($data = $result->fetch_object()) {
-      echo '<option value="' . $data->id_tipe . '">' . $data->no_ruang . '</option>';
-    }
+//     while ($data = $result->fetch_object()) {
+//       echo '<option value="' . $data->id_tipe . '">' . $data->no_ruang . '</option>';
+//     }
 
-    $result->free();
-    $db->close();
-  }
-?>
-<!-- Punya Patan -->
+//     $result->free();
+//     $db->close();
+//   }
+// ?>
+<!-- Punya Patan -->  
 <?php 
     require_once("db_login.php");
 
