4f624721a70fcd0265f2f5fd470a347614a579d7
imadearya <imadearya1@gmail.com>
2022-10-05 21:35:49 +0700

    Update daftar.php

--- daftar.php
+++ daftar.php
@@ -86,6 +86,7 @@
                             <br>
                             <button type="submit" class="btn btn-primary" name="submit">Submit</button><br><br>
                         </form>
+                        <script src="script.js"></script>
                         
                     </div>
                 </div>
@@ -103,7 +104,6 @@
   </section>
     
     <script src="ajax.js"></script>
-    <script type="text/javascript" src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
