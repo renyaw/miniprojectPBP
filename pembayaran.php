4fbaf4d27ec994b1f8b926b059747915e51e17d2
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-14 22:54:52 +0700

    reformat

--- pembayaran.php
+++ pembayaran.php
@@ -13,102 +13,99 @@
     session_start();
     require_once "db_login.php";
     ?>
-  <nav class="navbar" style="background-color:#F1A661;">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-            <img src="img\icon.png" alt="Logo" width="30" class="d-inline-block align-text-top">
-            Bebek Voice
-            </a>
-        </div>
+    <nav class="navbar" style="background-color:#F1A661;">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="img\icon.png" alt="Logo" width="30" class="d-inline-block align-text-top">
+        Bebek Voice
+        </a>
+      </div>
     </nav>
+
     <div class="container">
         <h1 class="text-center my-4 mb-5">Pembayaran</h1>
     </div>
+
     <div class="container">
-        <div class="row mt-2 mx-auto">
-            <div class="col-6">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img src="img\mandiri1.jpeg" width="150" height="150" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <p class="card-title fs-4">Bank Mandiri</p>
-                            <p class="card-text">A.N. Arya Gessangie Sugyanti Nora Faqih</p>
-                            <p class="card-text">Nomor Rekening : 24839248382094</p>
-                        </div>
-                        </div>
+      <div class="row mt-2 mx-auto">
+        <div class="col-6">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="img\mandiri1.jpeg" width="150" height="150" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <p class="card-title fs-4">Bank Mandiri</p>
+                        <p class="card-text">A.N. Arya Gessangie Sugyanti Nora Faqih</p>
+                        <p class="card-text">Nomor Rekening : 24839248382094</p>
+                    </div>
                     </div>
                 </div>
             </div>
-            <div class="card col-6">
-              <div class="card mt-3">
-                <div class="text  fs-5 lh-lg">
-                    <!-- ini buat masukin dari post -->
-                    <div class="card-header">
-                      <p class="fw-bold fs-5 ">Detail Pemesanan</p>
-                    </div>
-                    <div class="card-body">
-                    <?php
-                    $scnama = $_POST["nama"];
-                    $scnama = ucwords(strtolower($scnama));
-                    $email = $_SESSION["email"];
-                    $nama = test_input($scnama);
-                    $tipe = test_input($_POST["tipe"]);
-                    $ruang = test_input($_POST["ruang"]);
-
-                    //insert into db
-                    $input = $db->query(
-                        "insert into pesanan(nama,email,no_ruang,id_tipe) values('$nama','$email','$ruang','$tipe')"
-                    );
-                    //mengupdate status ruang yang sudah dipesan menjadi 1
-                    $update = $db->query(
-                        "update ruang set status=1 where no_ruang='$ruang'"
-                    );
-                    //mengambil semua data pada tabel tibe yang id_tipe sama dengan tipe
-                    $result = $db->query(
-                        "select * from tipe where id_tipe='$tipe'"
-                    );
-                    //update status ruang
-
-                    //memasukkan data dari login ke $data dalam bentuk array
-                    $data = mysqli_fetch_assoc($result);
-
-                    echo "Email&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;: " .
-                        $email .
-                        "</br>";
-                    echo "Nama&emsp;&emsp;&emsp;&ensp;&nbsp;&emsp;&nbsp;: " .
-                        $nama .
-                        "</br>";
-                    echo "Tipe Ruangan&emsp;&ensp;: " .
-                        $data["nama_tipe"] .
-                        "</br>";
-                    echo "Nomor Ruangan&nbsp;: " . $ruang . "</br>";
-                    echo "Harga&emsp;&emsp;&emsp;&ensp;&nbsp;&emsp;&nbsp;: Rp" .
-                        $data["harga"] .
-                        "</br>";
-                    ?>
-                    </div>
-                </div>
-              </div>
-                <p>Upload Bukti Pembayaran</p>
-                <div class="">
-                    <div class="input-group mb-3">
-                        <input type="file" class="form-control" id="inputGroupFile02">
-                        <label class="input-group-text" for="inputGroupFile02">Upload</label>
-                </div>
-
         </div>
-        <div class="d-md-flex justify-content-md-end mb-2">
-          <a href="bookroom.php"><button type="button" class="btn btn-success"> Submit</button></a>
-        </div>
-        <div class="row mx-auto">
         
+        <div class="card col-6">
+          <div class="card mt-3">
+            <div class="text  fs-5 lh-lg">
+                <!-- ini buat masukin dari post -->
+                <div class="card-header">
+                  <p class="fw-bold fs-5 ">Detail Pemesanan</p>
+                </div>
+                <div class="card-body">
+                <?php
+                $scnama = $_POST["nama"];
+                $scnama = ucwords(strtolower($scnama));
+                $email = $_SESSION["email"];
+                $nama = test_input($scnama);
+                $tipe = test_input($_POST["tipe"]);
+                $ruang = test_input($_POST["ruang"]);
+
+                //insert into db
+                $input = $db->query(
+                    "insert into pesanan(nama,email,no_ruang,id_tipe) values('$nama','$email','$ruang','$tipe')"
+                );
+                //mengupdate status ruang yang sudah dipesan menjadi 1
+                $update = $db->query(
+                    "update ruang set status=1 where no_ruang='$ruang'"
+                );
+                //mengambil semua data pada tabel tibe yang id_tipe sama dengan tipe
+                $result = $db->query(
+                    "select * from tipe where id_tipe='$tipe'"
+                );
+                //update status ruang
+
+                //memasukkan data dari login ke $data dalam bentuk array
+                $data = mysqli_fetch_assoc($result);
+
+                echo "Email&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;: " .
+                    $email .
+                    "</br>";
+                echo "Nama&emsp;&emsp;&emsp;&ensp;&nbsp;&emsp;&nbsp;: " .
+                    $nama .
+                    "</br>";
+                echo "Tipe Ruangan&emsp;&ensp;: " .
+                    $data["nama_tipe"] .
+                    "</br>";
+                echo "Nomor Ruangan&nbsp;: " . $ruang . "</br>";
+                "</br>";
+                ?>
+                </div>
+            </div>
+          </div>
+              <p>Upload Bukti Pembayaran</p>
+          <div class="">
+              <div class="input-group mb-3">
+                  <input type="file" class="form-control" id="inputGroupFile02">
+                  <label class="input-group-text" for="inputGroupFile02">Upload</label>
+            </div>
+          </div>
+          <div class="d-md-flex justify-content-md-end mb-2">
+            <a href="bookroom.php"><button type="button" class="btn btn-success"> Submit</button></a>
+          </div>
         </div>
+      </div>
     </div>
-    
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
